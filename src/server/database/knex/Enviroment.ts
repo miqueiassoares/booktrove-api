fb649a5e1aea5d2b070ca817d10463f4d7cf3b42
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-09-25 01:10:23 -0300

    fix: build problem

--- src/server/database/knex/Enviroment.ts
+++ src/server/database/knex/Enviroment.ts
@@ -23,3 +23,18 @@ export const test: Knex.Config = {
   ...development,
   connection: ':memory:',
 };
+
+export const production: Knex.Config = {
+  client: 'pg',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations')
+  }, 
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: {rejectUnauthorized: false}
+  }
+};
