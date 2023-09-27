c394540f4fd8c8b8f211ed2c7a0d401e4b83e4db
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-09-26 21:30:38 -0300

    feat: added cors for application

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 
 const server = express();
+
+server.use(cors({
+  origin: process.env.ENABLED?.split(';') || []
+}));
 
 server.use(express.json());
 
